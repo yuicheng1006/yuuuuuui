63d956dab3d3f6d0491978e12662ff7113031103
Yui Cheng <yuice1006@gmail.com>
2025-11-25 01:19:23 +0800

    [main] version 1.0.8

--- config-global.js
+++ config-global.js
@@ -7,7 +7,7 @@ import packageJson from '@/package.json';
 export const CONFIG = {
   appName: 'Yuuuuuui',
   appVersion: packageJson.version,
-  assetsDir: process.env.NEXT_PUBLIC_ASSETS_DIR ?? '',
+  // assetsDir: process.env.NEXT_PUBLIC_ASSETS_DIR ?? '',
   // serverUrl: process.env.NEXT_PUBLIC_SERVER_URL ?? '',
   // hostUrl: process.env.NEXT_PUBLIC_HOST_URL ?? '',
 };
